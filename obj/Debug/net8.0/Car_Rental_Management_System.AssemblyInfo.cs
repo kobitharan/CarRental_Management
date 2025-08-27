5e80fb77b062d180bc3ef1a771013ad529fed932
vigetharan <vigetharan9@gmail.com>
2025-08-27 10:04:48 +0530

    27.08.2025

--- obj/Debug/net8.0/Car_Rental_Management_System.AssemblyInfo.cs
+++ obj/Debug/net8.0/Car_Rental_Management_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Car_Rental_Management_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0d451acf25a0ad4cf426f85076ecb354d804cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ce9d10d5baed168f7c0105dff70e2c4de387ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Car_Rental_Management_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Car_Rental_Management_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
