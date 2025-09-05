39cca7524fbdf44daa47ef76c08536be15f5b54c
vigetharan <vigetharan9@gmail.com>
2025-09-05 01:55:23 -0700

    register and home page updated

---
+++ Areas/Customer/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Car_Rental_Management_System.Models; 
+
+
+namespace Car_Rental_Management_System.Areas.Customer.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Register
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(UserRegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Your registration logic here
+
+            return RedirectToAction("Index", "Home"); // Or wherever you want
+        }
+    }
+}
+
---
+++ Models/UserRegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental_Management_System.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Confirm Password")]
+        public string PasswordConfimed { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Compare("Email", ErrorMessage = "Emails do not match.")]
+        [Display(Name = "Confirm Email")]
+        public string EmailConfirmed { get; set; }
+
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
